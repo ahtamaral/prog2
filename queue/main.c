c8228db34601ae2ee1e6910707ef7851a8a9a4cb
artur_amaral <artur.amaral@mettainnovations.com.br>
2023-06-22 14:44:05 -0300

    .

--- queue/main.c
+++ queue/main.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Node structure for doubly linked list
-typedef struct Node {
+/* Nó que representa um elemento da lista*/
+typedef struct Node 
+{
     char data;
     struct Node* prev;
     struct Node* next;
 } Node;
 
-// Queue structure
-typedef struct Queue {
-    Node* front;
+// Estrutura que representa a lista
+typedef struct _FILA 
+{
+    Node* front;    
     Node* rear;
-} Queue;
+} FILA;
 
-// Function to create a new node
-Node* createNode(char data) {
+// Cria nó a partir de um caracter
+Node* createNode(char data) 
+{
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = data;
     newNode->prev = NULL;
@@ -23,115 +26,128 @@ Node* createNode(char data) {
     return newNode;
 }
 
-// Function to initialize the queue
-Queue* createQueue() {
-    Queue* queue = (Queue*)malloc(sizeof(Queue));
-    queue->front = NULL;
-    queue->rear = NULL;
-    return queue;
+// Cria fila vazia
+FILA* criaFila() 
+{
+    FILA* fila = (FILA*)malloc(sizeof(FILA));
+    fila->front = NULL;
+    fila->rear = NULL;
+    return fila;
 }
 
-// Function to check if the queue is empty
-int isEmpty(Queue* queue) {
-    return queue->front == NULL;
+// Checa se fila está vazia
+int isEmpty(FILA* fila) 
+{
+    return fila->front == NULL;
 }
 
-// Function to enqueue (insert) an element at the back of the queue
-void enqueue(Queue* queue, char data) {
+// Insere elemento no final da fila
+void insert(FILA* fila, char data) 
+{
     Node* newNode = createNode(data);
     
-    if (isEmpty(queue)) {
-        queue->front = newNode;
-        queue->rear = newNode;
+    if (isEmpty(fila)) 
+    {
+        fila->front = newNode;
+        fila->rear = newNode;
     } else {
-        newNode->prev = queue->rear;
-        queue->rear->next = newNode;
-        queue->rear = newNode;
+        newNode->prev = fila->rear;
+        fila->rear->next = newNode;
+        fila->rear = newNode;
     }
     
-    printf("Enqueued element: %c\n", data);
+    printf("Inseriu elemento na fila: %c\n", data);
 }
 
-// Function to dequeue (remove) the front element of the queue
-void dequeue(Queue* queue) {
-    if (isEmpty(queue)) {
-        printf("Queue is empty. Cannot dequeue.\n");
+// remover elemento do inicio da fila
+void remover(FILA* fila) 
+{
+    if (isEmpty(fila)) 
+    {
+        printf("Fila vazia. Nao ha elementos para remover.\n");
         return;
     }
     
-    Node* frontNode = queue->front;
+    Node* frontNode = fila->front;
     char data = frontNode->data;
     
-    queue->front = frontNode->next;
+    fila->front = frontNode->next;
     
-    if (queue->front == NULL) {
-        queue->rear = NULL;
-    } else {
-        queue->front->prev = NULL;
+    if (fila->front == NULL) 
+    {
+        fila->rear = NULL;
+    } else 
+    {
+        fila->front->prev = NULL;
     }
     
     free(frontNode);
     
-    printf("Dequeued element: %c\n", data);
+    printf("removeu elemento: %c\n", data);
 }
 
-// Function to print the queue elements
-void printQueue(Queue* queue) {
-    if (isEmpty(queue)) {
-        printf("Queue is empty. No elements to print.\n");
+void printaFila(FILA* fila) 
+{
+    if (isEmpty(fila)) 
+    {
+        printf("Fila vazia. Nao ha elementos para printar.\n");
         return;
     }
     
-    Node* currentNode = queue->front;
+    Node* currentNode = fila->front;
     
-    printf("Queue elements: ");
-    while (currentNode != NULL) {
+    printf("Fila: ");
+    while (currentNode != NULL) 
+    {
         printf("%c ", currentNode->data);
         currentNode = currentNode->next;
     }
     printf("\n");
 }
 
-// Macro for maximum queue size
-#define MAX_QUEUE_SIZE 10
-
 int main() {
-    Queue* queue = createQueue();
+    FILA* fila = criaFila();
     
-    enqueue(queue, 'A');
-    enqueue(queue, 'B');
-    enqueue(queue, 'C');
-    printQueue(queue);
+    insert(fila, 'A');
+    insert(fila, 'B');
+    insert(fila, 'C');
+    printaFila(fila);
+    puts("");
+
+    remover(fila);
+    printaFila(fila);
+    puts("");
+
+    insert(fila, 'D');
+    insert(fila, 'E');
+    insert(fila, 'F');
+    printaFila(fila);
+    puts("");
+
+    remover(fila);
+    remover(fila);
+    remover(fila);
+    remover(fila);
+    remover(fila);
+    remover(fila);
+    printaFila(fila);
+    puts("");
     
-    dequeue(queue);
-    printQueue(queue);
+    insert(fila, 'G');
+    insert(fila, 'H');
+    insert(fila, 'I');
+    insert(fila, 'J');
+    insert(fila, 'K');
+    printaFila(fila);
+    puts("");
     
-    enqueue(queue, 'D');
-    enqueue(queue, 'E');
-    enqueue(queue, 'F');
-    printQueue(queue);
-    
-    dequeue(queue);
-    dequeue(queue);
-    dequeue(queue);
-    dequeue(queue);
-    dequeue(queue);
-    dequeue(queue);
-    printQueue(queue);
-    
-    enqueue(queue, 'G');
-    enqueue(queue, 'H');
-    enqueue(queue, 'I');
-    enqueue(queue, 'J');
-    enqueue(queue, 'K');
-    printQueue(queue);
-    
-    dequeue(queue);
-    dequeue(queue);
-    dequeue(queue);
-    dequeue(queue);
-    dequeue(queue);
-    printQueue(queue);
+    remover(fila);
+    remover(fila);
+    remover(fila);
+    remover(fila);
+    remover(fila);
+    printaFila(fila);
+    puts("");
     
     return 0;
 }
